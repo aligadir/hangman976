e69d42e0fef6581068aae060b08c866acd8958fd
aligadir <agg2206@outlook.com>
2024-12-12 15:31:10 +0000

    Update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -1,3 +1,11 @@
-favorite_fruits = ['apple', 'banana', 'cherry', 'grape', 'orange']
-word_list = favorite_fruits
-print(word_list)
+import random 
+
+word_list = ['apple', 'banana', 'cherry', 'mango', 'pear']
+
+print("Word list:", word_list)
+
+random_word = random.choice(word_list)
+
+
+print("Randomly selected word:", random_word)
+
