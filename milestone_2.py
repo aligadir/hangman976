f719c792a03a334b103e6424b010168d14d90d6b
aligadir <agg2206@outlook.com>
2024-12-12 15:52:54 +0000

    Update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -1,11 +1,19 @@
-import random 
+import random  # Import the random module to randomly select a word
 
 word_list = ['apple', 'banana', 'cherry', 'mango', 'pear']
 
-print("Word list:", word_list)
-
-random_word = random.choice(word_list)
+print("List of words:", word_list)
 
 
-print("Randomly selected word:", random_word)
+word = random.choice(word_list)
 
+
+print("Randomly selected word:", word)
+
+guess = input("Enter a single letter: ").lower()  # Convert input to lowercase
+
+
+if len(guess) == 1 and guess.isalpha():
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input. Please enter a single letter.")
